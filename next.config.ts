440f63c2ac36c602ab157dc7acc790e9dbf168d7
s4474581 <s4474581@gmail.com>
2025-11-06 00:54:41 +0900

    fix next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export", // ← これが重要！
+  basePath: "/s4474581-github-io-trial", // ← GitHub Pagesのリポジトリ名に合わせる
   images: {
-    unoptimized: true, // ← next exportでは画像最適化を無効化
+    unoptimized: true,
   },
-  basePath: `/${process.env.GITHUB_REPOSITORY?.split("/")[1] || ""}`,
+  // output: "export", // ← これが重要！
+  // images: {
+  //   unoptimized: true, // ← next exportでは画像最適化を無効化
+  // },
+  // basePath: `/${process.env.GITHUB_REPOSITORY?.split("/")[1] || ""}`,
 };
 
 export default nextConfig;
